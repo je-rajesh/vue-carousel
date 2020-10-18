2bf0763a0699e88183e3b01c793332e07acdd062
i_raj3sh <rajesh.rxca@gmail.com>
2020-10-18 13:16:04 +0530

    made a carousel in vue component on the top of flickity

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,37 @@
+<template>
+    <!-- <div data-flickity='{"cellAlign":"left","contain":true,"wrapAround":true}'> -->
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import Flickity from 'flickity';
+    import 'flickity/dist/flickity.min.css';
+
+    export default { 
+        
+        props: {
+            wraparound: {default: true},
+            autoplay: {default: false},
+            contain: { default: false },
+            cellAlign: {default: 'left'},
+            imagesLoaded: {default: false},
+            percentPosition: {default: false},
+            draggable: {default: true}
+        },
+
+        mounted() {
+            new Flickity(this.$el, {
+                cellAlign: this.cellAlign,
+                contain: this.contain, 
+                autoPlay: this.autoplay,
+                wrapAround: this.wraparound,
+                imagesLoaded: this.imagesLoaded,
+                percentPosition: this.percentPosition,
+                draggable: this.draggable,
+
+            });
+        }
+    }
+</script>
